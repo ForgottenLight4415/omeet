6adeac7b6421752dae7355933ae8b81684cf9ab0
crv44 <crv4415@gmail.com>
2022-08-07 17:57:13 +0530

    chore: call service updated

--- lib/data/providers/app_server_provider.dart
+++ lib/data/providers/app_server_provider.dart
@@ -17,10 +17,14 @@ class AppServerProvider {
   final int notFound = 404;
   final int error = 500;
 
-  Future<DecodedResponse> postRequest({required String path, required Map<String, dynamic> data}) async {
+  Future<DecodedResponse> postRequest(
+      {required String path, required Map<String, dynamic> data}) async {
     final Response _response = await post(
       Uri.https(AppStrings.baseUrl, AppStrings.subDirectory + path),
-      headers: <String, String>{"Content-Type": "application/json; charset=UTF-8", "Accept": "application/json",},
+      headers: <String, String>{
+        "Content-Type": "application/json; charset=UTF-8",
+        "Accept": "application/json",
+      },
       body: jsonEncode(data),
     );
     final DecodedResponse _decodedResponse = DecodedResponse(
@@ -32,15 +36,17 @@ class AppServerProvider {
   }
 
   Future<bool> callRequest(Map<String, dynamic> data) async {
-    final Response _response = await get(
+    final Response _response = await post(
       Uri.https(
-        AppStrings.bridgeCallBaseUrl,
-        AppStrings.voiceCallUrl,
-        data,
+        AppStrings.bridgeCallBaseUrlNew,
+        AppStrings.voiceCallUrlNew,
       ),
       headers: <String, String>{
         "Accept": "application/json",
+        "Content-Type": "application/json; charset=UTF-8",
+        "access-key": AppStrings.callTokenNew,
       },
+      body: jsonEncode(data),
     );
     return _response.statusCode == successCode;
   }
