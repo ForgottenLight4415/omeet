f76f0277b72cd0d10ddb7ec46449f2bb7c4c5c37
Vishal Pednekar <crv4415@gmail.com>
2022-10-24 14:34:09 +0530

    fixed build issues

--- lib/views/landing.dart
+++ lib/views/landing.dart
@@ -118,7 +118,7 @@ class LandingPage extends StatelessWidget {
             child: Align(
               alignment: Alignment.center,
               child: Text(
-                "BAGIC MCI\nVersion 1.0.59 (Build 59)",
+                "BAGIC MCI\nVersion 1.0.60 (Build 60)",
                 textAlign: TextAlign.center,
               ),
             ),
