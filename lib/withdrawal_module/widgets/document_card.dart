847a29beb41f5cf4231da6ec08cba1896e6c13df
Vishal Pednekar <61105050+ForgottenLight4415@users.noreply.github.com>
2023-09-09 15:24:24 +0530

    version update

--- lib/withdrawal_module/widgets/document_card.dart
+++ lib/withdrawal_module/widgets/document_card.dart
@@ -1,14 +1,20 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flowder_v2/flowder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+import '../data/providers/document_provider.dart';
+import '../utilities/show_snackbars.dart';
 import '../views/documents/doc_viewer.dart';
 import '../views/documents/video_player.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../widgets/scaling_tile.dart';
 import '../data/models/document.dart';
 
-class DocumentCard extends StatelessWidget {
+class DocumentCard extends StatefulWidget {
   final dynamic document;
   final DocumentType type;
 
@@ -16,30 +22,58 @@ class DocumentCard extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<DocumentCard> createState() => _DocumentCardState();
+}
+
+class _DocumentCardState extends State<DocumentCard> {
+  late final String _savePath;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    _setPath();
+    if (!mounted) return;
+  }
+  void _setPath() async {
+    Directory? _path = await getExternalStorageDirectory();
+    String _localPath = _path!.path + Platform.pathSeparator + 'Download';
+    final savedDir = Directory(_localPath);
+    bool hasExisted = await savedDir.exists();
+    if (!hasExisted) {
+      savedDir.create();
+    }
+    _savePath = _localPath;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 8.w),
       child: ScalingTile(
         onPressed: () async {
-          if (type != DocumentType.video && type != DocumentType.audio) {
+          if (widget.type != DocumentType.video && widget.type != DocumentType.audio) {
             Navigator.pushNamed(
               context,
               '/view/document',
               arguments: DocumentViewPageArguments(
-                (document as Document).fileUrl,
-                type,
+                (widget.document as Document).fileUrl,
+                widget.type,
               ),
             );
           } else {
             Navigator.pushNamed(
-              context,
-              '/view/audio-video',
-              arguments: VideoWebViewArguments(
-                  type == DocumentType.video
-                    ? (document as Document).fileUrl
-                    : (document as AudioRecording).callUrl,
-                  type,
-              )
+                context,
+                '/view/audio-video',
+                arguments: VideoWebViewArguments(
+                  widget.type == DocumentType.video
+                      ? (widget.document as Document).fileUrl
+                      : (widget.document as AudioRecording).callUrl,
+                  widget.type,
+                )
             );
           }
         },
@@ -62,8 +96,8 @@ class DocumentCard extends StatelessWidget {
                     minWidth: 110.w,
                   ),
                   decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(14.0),
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(14.0),
                   ),
                   child: Center(
                     child: FaIcon(
@@ -79,7 +113,7 @@ class DocumentCard extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Text(
-                        (document as Document).fileType,
+                        (widget.document as Document).fileType,
                         textAlign: TextAlign.left,
                         overflow: TextOverflow.fade,
                         maxLines: 1,
@@ -88,43 +122,54 @@ class DocumentCard extends StatelessWidget {
                       ),
                       SizedBox(height: 10.h),
                       Text(
-                        (document as Document).fileName,
+                        (widget.document as Document).fileName,
                         textAlign: TextAlign.left,
                         overflow: TextOverflow.ellipsis,
                         softWrap: true,
                         maxLines: 2,
                         style: Theme.of(context).textTheme.bodyMedium,
                       ),
-                      type == DocumentType.audio
+                      widget.type == DocumentType.audio
                           ? Text(
-                              "From: " + document.callFrom,
-                              textAlign: TextAlign.left,
-                              overflow: TextOverflow.fade,
-                              maxLines: 2,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            )
+                        "From: " + widget.document.callFrom,
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.fade,
+                        maxLines: 2,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      )
                           : const SizedBox(),
-                      type == DocumentType.audio
+                      widget.type == DocumentType.audio
                           ? Text(
-                              "To: " + document.callTo,
-                              textAlign: TextAlign.left,
-                              overflow: TextOverflow.fade,
-                              maxLines: 2,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            )
+                        "To: " + widget.document.callTo,
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.fade,
+                        maxLines: 2,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      )
                           : const SizedBox(),
                       SizedBox(height: 12.h),
                       Text(
-                        document.uploadDateTime,
+                        widget.document.uploadDateTime,
                         textAlign: TextAlign.left,
                         overflow: TextOverflow.fade,
                         maxLines: 2,
                         style: Theme.of(context).textTheme.bodyMedium,
                       ),
-                      TextButton(
-                        onPressed: () => showDescription(context),
-                        child: const Text("Show Details"),
-                      )
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          TextButton(
+                            onPressed: () => showDescription(context),
+                            child: const Text("Show Details"),
+                          ),
+                          widget.type != DocumentType.audio
+                              ? TextButton(
+                            onPressed: () => downloadDocument(context),
+                            child: const Text("Download"),
+                          )
+                              : const SizedBox(),
+                        ],
+                      ),
                     ],
                   ),
                 ),
@@ -137,9 +182,9 @@ class DocumentCard extends StatelessWidget {
   }
 
   IconData _getCardIcon() {
-    switch (type) {
+    switch (widget.type) {
       case DocumentType.file:
-        if (document.fileUrl.split(".").last == "pdf") {
+        if (widget.document.fileUrl.split(".").last == "pdf") {
           return FontAwesomeIcons.fileLines;
         }
         return FontAwesomeIcons.fileImage;
@@ -153,9 +198,9 @@ class DocumentCard extends StatelessWidget {
   }
 
   String _getCardTitle() {
-    switch (type) {
+    switch (widget.type) {
       case DocumentType.file:
-        if (document.fileUrl.split(".").last == "pdf") {
+        if (widget.document.fileUrl.split(".").last == "pdf") {
           return "Document";
         }
         return "Image";
@@ -168,31 +213,57 @@ class DocumentCard extends StatelessWidget {
     }
   }
 
+  void downloadDocument(BuildContext context) async {
+    final options = DownloaderUtils(
+      progressCallback: (current, total) {
+        final progress = (current / total) * 100;
+        log('Downloading: $progress');
+        log('Total: $total');
+        log('Current: $current');
+      },
+      file: File('$_savePath/${(widget.document as Document).fileName}'),
+      progress: ProgressImplementation(),
+      onDone: () {
+        showInfoSnackBar(
+          context,
+          "Download completed - $_savePath/${(widget.document as Document).fileName}",
+          color: Colors.green,
+        );
+        log('$_savePath/${(widget.document as Document).fileName}');
+      },
+      deleteOnCancel: true,
+    );
+    await Flowder.download(
+      await DocumentProvider().getDocument((widget.document as Document).fileUrl),
+      options,
+    );
+  }
+
   void showDescription(BuildContext context) async {
     await showDialog(
-        context: context,
-        builder: (context) => SimpleDialog(
-          title: const Text("Details"),
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: SingleChildScrollView(
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text("Type: ${_getCardTitle()}"),
-                      const SizedBox(height: 16.0),
-                      Text((document as Document).fileDesc),
-                    ],
-                  ),
+      context: context,
+      builder: (context) => SimpleDialog(
+        title: const Text("Details"),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: SingleChildScrollView(
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Type: ${_getCardTitle()}"),
+                    const SizedBox(height: 16.0),
+                    Text((widget.document as Document).fileDesc),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
